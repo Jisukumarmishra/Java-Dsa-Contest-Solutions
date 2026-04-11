fe1d0e4f78479deaaa926465aeb3bd3e2fe7cb14
mishra <jisumishra4@gmail.com>
2026-04-11 19:43:09 +0530

    Excited To Biweekly Contest 180

--- CodeCheff/STARTERS 233/Movies.java
+++ CodeCheff/STARTERS 233/Movies.java
@@ -2,26 +2,24 @@ import java.util.*;
 
 class Codechef {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+      Scanner sc = new Scanner(System.in);
 
-        int t = sc.nextInt();
+      int t = sc.nextInt();
 
-        while (t-- > 0) {
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int c = sc.nextInt();
+      while (t-- > 0) {
+      int n = sc.nextInt();
+      int m = sc.nextInt();
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      int c = sc.nextInt();
 
-            int combos = Math.min(n, m);
+      int combos = Math.min(n, m);
 
-            int cost = combos * c 
-                     + (n - combos) * a 
-                     + (m - combos) * b;
-
-            System.out.println(cost);
-        }
-    }
+      int cost = combos*c + (n - combos) * a  + (m - combos) * b;
+      
+      System.out.println(cost);
+      }
+  }
 }
 
 // import java.util.*;
